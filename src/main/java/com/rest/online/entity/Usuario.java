d4695c1c5af423b45a2ea50ac4925643f1ff3b45
Guillermo <guillepower2001@gmail.com>
2022-04-07 10:05:33 -0500

    Pagina maven 1

---
+++ src/main/java/com/rest/online/entity/Usuario.java
@@ -0,0 +1,86 @@
+package com.rest.online.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private Integer codigo;
+    @Column(name = "nombre_usuario", length = 20, nullable = true)
+    private String nombreUsuario;
+    @Column(name = "contrase\u00f1a", length = 20, nullable = true)
+    private String contraseña;
+    @Column(name = "nombre", length = 20, nullable = true)
+    private String nombre;
+    @Column(name = "sexo", length = 15, nullable = true)
+    private String sexo;
+    @Column(name = "edad")
+    private Integer edad;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+}
